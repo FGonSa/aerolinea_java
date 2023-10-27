58965cc3f23565a0cd762161344f3488031ffef1
Frank Santos <fransecre@gmail.com>
2023-10-27 22:25:33 +0200

    Separada la lógica de programación

---
+++ aerolinea/Constantes.java
@@ -0,0 +1,14 @@
+package aerolinea;
+
+public class Constantes {
+	final static int MIN_OPTION_MAIN_MENU = 0;
+	final static int MAX_OPTION_MAIN_MENU = 2;
+	
+	final static int TRABAJADORES_LENGTH = 4;
+
+	final static int MIN_OPTION_ADMIN_MENU = 0;
+	final static int MAX_OPTION_ADMIN_MENU = 3;
+
+	final static String ADMIN_USER = "123";
+	final static String ADMIN_PASS = "123";
+}
